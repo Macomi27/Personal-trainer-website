8692733baddc5d67137892351059535eb9eb64e2
Macomi27 <macomfortitcham@gmail.com>
2025-06-27 12:29:49 +0100

    navbar desktop complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import './Header.css'
+import siteLogo from './assets/logo.png'
+import { FaCartShopping } from "react-icons/fa6";
 
 const Header = () => {
     return(
-
-        <p>This is the header</p>
+        <nav>
+            <div className="left-nav">
+                <div className="logo">
+                    <img src={siteLogo} alt="Personal Trainer site Logo" />
+                </div>
+                <p className="logoname">John Smith</p>
+            </div>
+            <div className="right-nav">
+            <ul className="navList">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Offer</a></li>
+                <li><a href="/">Price list</a></li>
+                <li><a href="/">About me</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+            <FaCartShopping className="cart"/> 
+            </div>
+        </nav>
     )
 }
 
