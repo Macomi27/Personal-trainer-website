88b7e7c45a85b2f613b0ce2a485454d2a6617a1b
Macomi27 <macomfortitcham@gmail.com>
2025-06-29 19:15:29 +0100

    Hero section desktop setup

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return(
+        <div className="heroSection">
+            <div className="heroElements">
+                <h2>Personal Trainer</h2>
+                <p>I will help you achieve the intended results</p>
+                <button>free training</button>
+            </div>    
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header.jsx'
 import Footer from '../components/Footer/Footer.jsx';
+import Hero from '../components/Hero/Hero.jsx';
 
 const Home = () => {
     return(
         <>
-            {/* <Header /> */}
+            <Header />
+            <Hero />
             <Footer />
         </>
     )
