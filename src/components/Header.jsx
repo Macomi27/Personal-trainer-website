3766b36344ff2f09bfc223d73c77b6d0598a6df4
Macomi27 <macomfortitcham@gmail.com>
2025-06-29 03:10:06 +0100

    Responsive navbar setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import './Header.css'
 import siteLogo from './assets/logo.png'
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCartShopping, FaBars } from "react-icons/fa6";
+
+// let bars = document.getElementById("bars");
+// let mobileStatus = false
+// // let mobileMenu = document.getElementById("mobile-nav-links");
+
+// // Function to handle mobile navigation toggle
+// function handleMobileNav(){
+//         mobileStatus = !mobileStatus
+//         console.log(mobileStatus)
+//     if (!mobileStatus){
+//         //
+//     }
+// }
+
+// bars.addEventListener("click", handleMobileNav);
 
 const Header = () => {
     return(
         <nav>
+            <div className="nav-container">
             <div className="left-nav">
                 <div className="logo">
                     <img src={siteLogo} alt="Personal Trainer site Logo" />
@@ -20,8 +36,20 @@ const Header = () => {
                 <li><a href="/">About me</a></li>
                 <li><a href="/">Contact</a></li>
             </ul>
+            {/* <div className="mobile-nav"> */}
             <FaCartShopping className="cart"/> 
+
+            <FaBars className="hamburger" id="bars" />
+            {/* </div> */}
             </div>
+            </div>
+            <ul className="mobile-nav-links" id="mobile-nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Offer</a></li>
+                <li><a href="/">Price list</a></li>
+                <li><a href="/">About me</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
         </nav>
     )
 }
